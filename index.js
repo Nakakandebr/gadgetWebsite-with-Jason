41d5de0e620ea9cdd1b420ab9e5347af9f132000
Nakakandebr <bridgetnakakandea@gmail.com>
2023-06-12 10:17:02 +0300

    commits

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-function fetchProducts() {
-    fetch("https://dummyjson.com/products")
-      .then((response) => response.json())
-      .then((collection) => {
-        const returnedProduct = collection.products.slice(3, 9);
-        getProductCards(returnedProduct);
-      });
-  }
+// function fetchProducts() {    // fetch("https://dummyjson.com/products")      // .then((response) => response.json())      // .then((collection) => {        // const returnedProduct = collection.products.slice(3, 9);        // getProductCards(returnedProduct);      // });  // }// // function getProductCards(collection) {    // const productContainer = document.getElementById("products");    // for (const product of collection) {      // const specificProduct = document.createElement("div");      // specificProduct.classList.add("product-card");      // specificProduct.innerHTML = `        // <img src="${product.thumbnail}" alt="${product.name}" />        // <h2>${product.title}</h2>        // <div class='productPrice'>          // <p>${product.price}</p>          // <button>Add to Cart</button>        // </div>      // `;      // productContainer.appendChild(specificProduct);    // }  // }  //   // fetchProducts();
+let  productsContainer=document.getElementById('products')
+const getProducts=()=>{
+    return fetch('https://dummyjson.com/products?limit=8')
+    .then(response => response.json())
+    .then(response=>response)
+    .catch(error=>error)
+}
+getProducts();
 
-function getProductCards(collection) {
-    const productContainer = document.getElementById("products");
-    for (const product of collection) {
-      const specificProduct = document.createElement("div");
-      specificProduct.classList.add("product-card");
-      specificProduct.innerHTML = `
-        <img src="${product.thumbnail}" alt="${product.name}" />
-        <h2>${product.title}</h2>
-        <div class='productPrice'>
-          <p>${product.price}</p>
-          <button>Add to Cart</button>
-        </div>
-      `;
-      productContainer.appendChild(specificProduct);
-    }
-  }
-  
-  fetchProducts();
+
+const displayProducts=async()=>{
+    const products=await getProducts()
+    console.log(products);
+    products.products.map(item=>{
+        console.log(item);
+
+        let div=document.createElement('div');
+        let images=document.createElement('img');
+        let title=document.createElement('h2');
+        let brand=document.createElement('p');
+        let price=document.createElement('p');
+        let ratings=document.createElement('h4');
+
+
+        images.src=item.thumbnail
+        title.innerHTML=item.title;
+        brand.innerHTML=item.brand
+        price.innerHTML=item.price
+        ratings.innerHTML=item.rating
+
+        
+        div.appendChild(images)
+        div.appendChild(title)
+        div.appendChild(brand)
+        div.appendChild(price)
+        div.appendChild(ratings)
+        div.setAttribute('key',item.id);
+        div.setAttribute('class','product');
+        productsContainer.appendChild(div)
+    })
+}
+displayProducts()
